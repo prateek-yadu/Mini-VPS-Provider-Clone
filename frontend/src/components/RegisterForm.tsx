a8a064c320abcc1b0e70e5fbb7f69ebfe30075c7
Prateek Yadu <prateek@prateekyadu.com>
2026-02-27 17:56:50 +0530

    chore(frontend): re-designed login and register page

---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,103 @@
+import { Link, Navigate } from "react-router";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
+import type { RootState } from "../app/store";
+
+export function RegisterForm() {
+
+  // gets AuthState
+  const authState = useSelector((state: RootState) => state.AuthState);
+
+  // user input
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // register handling logic
+  const handleRegister = async (event: { preventDefault: () => void; }) => {
+    try {
+
+      event?.preventDefault();
+
+      // sends data to DB
+      const response = await (await fetch("/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: name, email: email, password: password })
+      })).json();
+
+      // clears input
+      setName("");
+      setEmail("");
+      setPassword("");
+
+      // shows toast
+      if (response.status == 201) {
+        toast.success(response.message);
+      } else if (response.status == 409) {
+        toast.error(response.message);
+      } else {
+        toast.error(response.message);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  if (authState.isAuthenticated) {
+    return <Navigate to={"/"} replace={true} />;
+  }
+
+  return (
+    <form onSubmit={handleRegister}>
+      <h1 className="font-semibold text-3xl text-primary ">Get started</h1>
+      <span className="my-2 text-sm text-secondary-foreground">Create a new account</span>
+
+      {/* Oauth Register Option */}
+      <div className="mt-12 flex flex-col w-full gap-4">
+        <button className="bg-white border border-border-primary py-3 rounded text-primary cursor-pointer hover:bg-border-primary/[1%] hover:border-primary/20 font-medium text-sm flex items-center justify-center gap-4" type="button">
+          <span>
+            <img src="/images/logos/google.svg" alt="google-logo" className="size-8" />
+          </span>
+          Continue with Google</button>
+        <button className="bg-white border border-border-primary py-3 rounded text-primary cursor-pointer hover:bg-border-primary/[1%] hover:border-primary/20 font-medium text-sm flex items-center justify-center gap-4" type="button">
+          <span>
+            <img src="/images/logos/github.svg" alt="github-logo" className="size-8" />
+          </span>
+          Continue with Github</button>
+      </div>
+
+      {/* Seprator */}
+      <div className="my-5 relative flex items-center justify-center">
+        <span className="text-center text-accent bg-white px-4">or</span>
+        <div className="h-[1px] bg-muted/30 absolute top-3 w-full -z-10" />
+      </div>
+
+      {/* Email based Register Option */}
+      <div className="pb-4">
+        <label htmlFor="name" className="block text-sm">Name</label>
+        <input type="text" name="name" id="name" className="w-full py-1 px-3 my-1 border-[1px] rounded border-accent/20 focus:border-accent/40 outline-none bg-accent/[4%] text-sm " placeholder="John Doe" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="pb-4">
+        <label htmlFor="email" className="block text-sm">Email</label>
+        <input type="email" name="email" id="email" className="w-full py-1 px-3 my-1 border-[1px] rounded border-accent/20 focus:border-accent/40 outline-none bg-accent/[4%] text-sm " placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="py-1">
+        <label htmlFor="password" className="block text-sm">Password</label>
+        <input type="password" name="password" id="password" className="w-full py-1 px-3 my-1 border-[1px] rounded border-accent/20 focus:border-accent/40 outline-none bg-accent/[4%] text-sm " placeholder="••••••••" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+
+      {/* Register Button */}
+      <div className="py-8">
+        <button className="bg-accent py-3 text-center w-full text-white rounded cursor-pointer hover:bg-accent/95" type="submit">Register</button>
+        <p className="py-8 text-sm text-center">Already have an account? <Link to={"/login"} className="text-accent font-medium underline">Login</Link></p>
+      </div>
+
+    </form>
+  );
+}
--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,34 +1,23 @@
-import { GalleryVerticalEnd } from "lucide-react";
-import { LoginForm } from "../../components/login-form";
+import { LoginForm } from "../../components/LoginForm";
 import Branding from "../../branding.json";
 
 export default function Login() {
     const appName = Branding.AppName; // gets app name from @/src/branding.json
     return (
-        <div className="grid min-h-svh lg:grid-cols-2">
-            <div className="flex flex-col gap-4 p-6 md:p-10">
-                <div className="flex justify-center gap-2 md:justify-start">
-                    <a href="#" className="flex items-center gap-2 font-medium">
-                        <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                            <GalleryVerticalEnd className="size-4" />
-                        </div>
-                        {appName}
-                    </a>
-                </div>
-                <div className="flex flex-1 items-center justify-center">
-                    <div className="w-full max-w-sm">
-                        <LoginForm />
-                    </div>
-                </div>
-            </div>
-            <div className="bg-muted relative hidden lg:block">
-                <img
-                    src="/placeholder.svg"
-                    alt="Image"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-            </div>
-        </div>
+        <div className="grid grid-cols-2 h-screen bg-accent/[1%] ">
+            <div className="flex flex-col">
+                {/* Branding */}
+                <h2 className="logo font-semibold text-xl text-accent py-8 px-12">{appName}</h2>
 
+                <div className="w-[45%] m-auto">
+                    {/* Login Form  */}
+                    <LoginForm />
+                </div>
+
+            </div>
+
+            <div className="bg-accent"></div>
+
+        </div>
     );
 }
