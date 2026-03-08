b1141281361e7ed34111aed3b6a2b999ec9c6150
Prateek Yadu <prateek@prateekyadu.com>
2026-03-08 18:42:19 +0530

    feat(backend): added support for in_use & is_expired query

--- backend/controller/me.controller.ts
+++ backend/controller/me.controller.ts
@@ -1,6 +1,7 @@
 import { pool } from "../db/config.js";
 import send from "../utils/response/response.js";
 import { Request, Response } from "express";
+import { isExpired } from "../utils/validators/planValidators.js";
 
 interface customRequest extends Request {
     id?: string;
@@ -29,13 +30,52 @@ export const subscribedPlans = async (req: customRequest, res: Response) => {
 
     try {
         const id = req.id; // gets user id 
+        const { is_expired, in_use } = req.query; // gets user query
 
         // fetches user subscribed plans 
         const [user_plan]: any = await pool.query('SELECT u.id, u.in_use, u.purchased_at, u.expires_at, p.name, p.vCPU, p.memory, p.storage, p.backups FROM user_plans u INNER JOIN plans p ON u.plan_id=p.id WHERE u.user_id=?', [id]);
 
-        if (user_plan.length != 0) {
+        let filteredPlan = user_plan; // adds query filtering if required
+
+        // handles is_expired query
+        switch (is_expired) {
+            case "true":
+                filteredPlan = filteredPlan.filter((item: { expires_at: Date; }) => {
+                    const isPlanExpired: boolean = isExpired(item.expires_at);
+                    if (isPlanExpired) {
+                        return item;
+                    }
+                });
+                break;
+
+            case "false":
+                filteredPlan = filteredPlan.filter((item: { expires_at: Date; }) => {
+                    const isPlanExpired: boolean = isExpired(item.expires_at);
+                    if (!isPlanExpired) {
+                        return item;
+                    }
+                });
+
+            default:
+                break;
+        }
+
+        // handles in_use query
+        switch (in_use) {
+            case "true":
+                filteredPlan = filteredPlan.filter((item: { in_use: number; }) => item.in_use === 1);
+                break;
+
+            case "false":
+                filteredPlan = filteredPlan.filter((item: { in_use: number; }) => item.in_use === 0);
+
+            default:
+                break;
+        }
+
+        if (filteredPlan.length != 0) {
             // sends subscribed plan data 
-            send.ok(res, "Plan Found", user_plan);
+            send.ok(res, "Plan Found", filteredPlan);
         } else {
             send.notFound(res, "You are not subscribed to any plan.");
         }
