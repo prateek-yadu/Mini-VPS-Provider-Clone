f5f59b91fbacc1d01a854ed735ca3397742a2836
Prateek Yadu <prateek@prateekyadu.com>
2026-03-14 17:02:35 +0530

    chore(frontend): added responsiveness to login & register page

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,22 +1,29 @@
 import Branding from "../../branding.json";
-import { RegisterForm } from "../../components/RegisterForm";
+import { RegisterForm } from "../../components/auth/RegisterForm";
 
-export default function Login() {
-    const appName = Branding.AppName; // gets app name from @/src/branding.json
-    return (
-        <div className="grid grid-cols-2 h-screen bg-accent/[1%] ">
-            <div className="flex flex-col">
-                {/* Branding */}
-                <h2 className="logo font-semibold text-xl text-accent py-8 px-12">{appName}</h2>
+export default function Register() {
+  const appName = Branding.AppName; // gets app name from @/src/branding.json
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen bg-accent/[1%] ">
+      <div className="flex flex-col">
+        {/* Branding */}
+        <h2 className="logo font-semibold text-xl text-accent px-8 py-8 lg:px-12">
+          {appName}
+        </h2>
 
-                <div className="w-[45%] m-auto">
-                    {/* Register Form  */}
-                    <RegisterForm />
-                </div>
-            </div>
-
-            <div className="bg-accent"></div>
-
+        <div className="w-[70%] lg:w-[60%] xl:w-[45%] m-auto">
+          {/* Register Form  */}
+          <RegisterForm />
         </div>
-    );
+      </div>
+
+      <div className="hidden lg:flex">
+        <img
+          src="/images/auth.jpg"
+          alt=""
+          className="object-center object-cover"
+        />
+      </div>
+    </div>
+  );
 }
