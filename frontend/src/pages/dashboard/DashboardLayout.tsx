73c77c51657a1fbf1da90af714f8b670b049e3a4
Prateek Yadu <prateek@prateekyadu.com>
2026-03-16 02:33:03 +0530

    feat(frontend): made dashboard responsive

--- frontend/src/pages/dashboard/DashboardLayout.tsx
+++ frontend/src/pages/dashboard/DashboardLayout.tsx
@@ -1,13 +1,11 @@
-import { Bell, CircleQuestionMark, CircleUser, CreditCard, Home, LanguagesIcon, LayoutDashboard, LogOut, Server, Store } from "lucide-react";
-import Branding from "../../branding.json";
+import { Bell, CircleQuestionMark, CircleUser, CreditCard, Home, LanguagesIcon, LayoutDashboard, LogOut, Menu, Server, Store } from "lucide-react";
 import { Link, Outlet, useLocation } from "react-router";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../app/store";
+import Sidebar from "../../components/dashboard/Sidebar";
 
 export default function DashboardLayout() {
-
-  const appName = Branding.AppName; // gets app name from @/src/branding.json
 
   // gets user info
   const user = useSelector((state: RootState) => state.AuthState);
@@ -75,70 +73,51 @@ export default function DashboardLayout() {
 
   // get pathname eg. /dahboard/vps => VPS
   const currentURLRoute = location.pathname.split("/").pop();
-  
-  const fullURLPath = location.pathname // gets full url path eg. /dashboard/vps
 
   // handles user account collapasble button logic
   const [isCollapsableOpen, setIsCollapsableOpen] = useState(false);
 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // stores drawer state
+  const changeSidebarState = () => {
+    setIsSidebarOpen(true);
+  };
+
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       {/* sidebar */}
-      <aside className="h-screen bg-white w-[13%] p-4 relative">
-
-        {/* App title */}
-        <h2 className="logo font-semibold text-xl text-primary text-center">{appName}</h2>
-
-        {/* Dashboard links */}
-        <ul className="mt-6 space-y-1">
-          {links.map((link, index) => (
-            <li key={index}>
-              <Link to={link.href} className={`flex items-center gap-2 hover:text-accent hover:bg-accent/[6%] py-3 px-4 rounded text-sm ${fullURLPath === link.href? "text-accent bg-accent/[6%]": "text-secondary-foreground"}`}>
-                <link.icon className="size-5" />
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Footer Links */}
-        <div className="absolute bottom-2">
-          <ul className="flex flex-col">
-            {footerLinks.map((link, index) => (
-              <li key={index}>
-                <Link to={link.href} className="flex items-center gap-2 text-primary hover:text-accent py-3 px-4 text-sm">
-                  <link.icon className="size-5" />
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-      </aside>
+      <Sidebar headerLinks={links} footerLinks={footerLinks} isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
 
       {/* main pannel */}
       <div className="flex-1 bg-primary-background border-l-[1px] border-border-primary">
 
         {/* main pannel nav bar */}
-        <nav className="bg-white border-b-[1px] border-border-primary flex items-center justify-between px-8 py-2">
+        <nav className="bg-white border-b-[1px] border-border-primary flex items-center justify-between px-4 lg:px-8 py-2">
+
+          {/* Mobile menu  toggle */}
+          <div className="lg:hidden">
+            <button className="flex items-center cursor-pointer" onClick={() => { changeSidebarState(); }}>
+              <Menu className="size-6 text-accent" />
+            </button>
+          </div>
 
           {/* Breadcrumb */}
-          <div className="flex items-center text-sm text-secondary-foreground gap-2 uppercase">
+          <div className="hidden lg:block">
+            <div className="flex items-center text-sm text-secondary-foreground gap-2 uppercase">
 
-            <Link to={"/dashboard"} className=""><Home className="size-5 hover:text-accent" /></Link>
-            {currentURLRoute != "dashboard" &&
-              <>
-                <div className="">-</div>
-                <div className="">{currentURLRoute}</div>
-              </>
-            }
+              <Link to={"/dashboard"} className=""><Home className="size-5 hover:text-accent" /></Link>
+              {currentURLRoute != "dashboard" &&
+                <>
+                  <div className="">-</div>
+                  <div className="">{currentURLRoute}</div>
+                </>
+              }
+            </div>
           </div>
 
           <div className='flex items-center justify-center gap-4'>
 
             {/* user account info collapsable card */}
-            <div className={`${isCollapsableOpen ? "absolute" : "hidden"} top-14 right-16 bg-white shadow border-[1px] border-border-primary rounded z-50`}>
+            <div className={`${isCollapsableOpen ? "absolute" : "hidden"} top-14 right-16 bg-white shadow border-[1px] border-border-primary rounded z-50 w-80`}>
 
               {/* user name, image and email */}
               <div className="flex items-center gap-4 p-4">
@@ -173,7 +152,7 @@ export default function DashboardLayout() {
             <div className={`${isCollapsableOpen ? "absolute top-0 left-0 right-0 bottom-0" : "hidden"}`} onClick={() => { setIsCollapsableOpen(!isCollapsableOpen); }}></div>
 
             {/* user account button */}
-            <button className="size-9 rounded-full z-40 cursor-pointer" onClick={() => { setIsCollapsableOpen(!isCollapsableOpen); }}>
+            <button className="size-9 rounded-full cursor-pointer" onClick={() => { setIsCollapsableOpen(!isCollapsableOpen); }}>
               <img src={user.imageUrl} alt={fallbackUserAvatar}
                 className='w-full h-full object-cover rounded-full'
               />
@@ -183,7 +162,7 @@ export default function DashboardLayout() {
         </nav>
 
         {/* page content */}
-        <div className="px-8 py-4">
+        <div className="px-4 lg:px-8 py-4">
           <Outlet />
         </div>
 
