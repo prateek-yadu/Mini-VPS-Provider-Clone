649a07685ef026bbf4efa87f58864f6006faa72a
Prateek Yadu <prateek@prateekyadu.com>
2026-04-15 23:52:36 +0530

    chore(backend): relocated redis and mysql connection files to /backend/lib

---
+++ backend/lib/redis.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config({ path: "../.env" });
+
+export const redisConnection = {
+    connection: {
+        host: process.env.REDIS_SERVER, 
+        port: Number(process.env.REDIS_SERVER_PORT)
+    }
+}
