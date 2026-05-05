7b422ce13c8589e743bcd5595c4657e831640f55
Prateek Yadu <prateek@prateekyadu.com>
2026-05-05 14:46:24 +0530

    feat(backend/worker): implemented heartbeat on lifecycle worker

--- backend/workers/lifecycle.worker.ts
+++ backend/workers/lifecycle.worker.ts
@@ -4,112 +4,173 @@ import { Job, Worker } from "bullmq";
 import { redisConnection } from "../lib/redis.ts";
 // @ts-ignore
 import { pool } from "../lib/db.ts";
+import { Redis } from "ioredis";
 
 config({ path: "../.env" });
 
-const worker = new Worker(process.env.REDIS_LIFECYCLE_QUEUE || 'instance-lifecycle', async (job: Job) => {
+// new redis instance
+const redis = new Redis();
 
+var iscrashed = false;
+
+const worker = new Worker(
+  process.env.REDIS_LIFECYCLE_QUEUE || "instance-lifecycle",
+  async (job: Job) => {
     try {
+      switch (job.data.operation) {
+        case "start":
+        case "stop":
+        case "restart":
+          // gets instance
+          const getInstanceInfo: any = await (
+            await fetch(
+              `${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}`,
+              {
+                method: "GET",
+              },
+            )
+          ).json();
 
-        switch (job.data.operation) {
-            case "start":
-            case "stop":
-            case "restart":
+          // performs operation if instance exists
+          if (getInstanceInfo.status === 200) {
+            // if status is already set as operation then, skip # TODO
+            // do a req, if already in thet state skip # TODO
 
-                // gets instance
-                const getInstanceInfo: any = await (await fetch(`${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}`, {
-                    method: "GET"
-                })).json();
+            // req to change instance state
+            const instanceOperationReq: any = await (
+              await fetch(
+                `${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}/${job.data.operation}`,
+                { method: "PUT" },
+              )
+            ).json();
 
-                // performs operation if instance exists
-                if (getInstanceInfo.status === 200) {
+            if (job.data.operation === "restart") {
+              // update status to restarting
+              const [setStatusToRestarting]: any = await pool.query(
+                "UPDATE instances SET status=? WHERE id=?",
+                ["restarting", job.data.name],
+              );
+            }
 
-                    // if status is already set as operation then, skip # TODO
-                    // do a req, if already in thet state skip # TODO
+            if (job.data.operation === "start") {
+              // update status to starting
+              const [setStatusToStarting]: any = await pool.query(
+                "UPDATE instances SET status=? WHERE id=?",
+                ["starting", job.data.name],
+              );
+            }
 
-                    // req to change instance state
-                    const instanceOperationReq: any = await (await fetch(`${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}/${job.data.operation}`, { method: "PUT" })).json();
+            if (job.data.operation === "stop") {
+              // update status to stopping
+              const [setStatusToStopping]: any = await pool.query(
+                "UPDATE instances SET status=? WHERE id=?",
+                ["stopping", job.data.name],
+              );
+            }
 
-                    if (job.data.operation === "restart") {
-                        // update status to restarting
-                        const [setStatusToRestarting]: any = await pool.query('UPDATE instances SET status=? WHERE id=?', ["restarting", job.data.name]);
-                    }
+            if (instanceOperationReq.status !== 200) {
+              throw new Error("Operation failed");
+            }
+          } else {
+            throw new Error(
+              `Instance not present to perform ${job.data.operation} operation`,
+            );
+          }
 
-                    if (job.data.operation === "start") {
-                        // update status to starting
-                        const [setStatusToStarting]: any = await pool.query('UPDATE instances SET status=? WHERE id=?', ["starting", job.data.name]);
-                    }
+          break;
 
-                    if (job.data.operation === "stop") {
-                        // update status to stopping
-                        const [setStatusToStopping]: any = await pool.query('UPDATE instances SET status=? WHERE id=?', ["stopping", job.data.name]);
-                    }
+        case "delete":
+          // gets instance
+          const getInstanceReq: any = await (
+            await fetch(
+              `${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}`,
+              {
+                method: "GET",
+              },
+            )
+          ).json();
 
-                    if (instanceOperationReq.status !== 200) {
-                        throw new Error("Operation failed");
-                    }
-                } else {
-                    throw new Error(`Instance not present to perform ${job.data.operation} operation`);
-                }
+          // handles db cleanup in instsance is not in LXD server
+          if (getInstanceReq.status === 404) {
+            // cleanup
 
-                break;
+            // delete data from instance table
+            const [deleteInstance]: any = await pool.query(
+              "DELETE FROM instances WHERE id=?",
+              [job.data.name],
+            );
 
-            case "delete":
+            // release user plan from in_use
+            const [releaseUserPlan]: any = await pool.query(
+              "UPDATE user_plans SET in_use=0 WHERE id=?",
+              [job.data.planId],
+            );
 
-                // gets instance
-                const getInstanceReq: any = await (await fetch(`${process.env.LXD_AGENT_SERVER}/api/v1/instance/${job.data.name}`, {
-                    method: "GET"
-                })).json();
+            // release reserved IP
+            const [releaseIP]: any = await pool.query(
+              "UPDATE ip_addresses SET in_use=0 WHERE id=?",
+              [job.data.instanceIPID],
+            );
+          } else {
+            // --- ignore when retry
 
-                // handles db cleanup in instsance is not in LXD server
-                if (getInstanceReq.status === 404) {
-                    // cleanup
+            if (job.attemptsMade < 1) {
+              // send data to lxd agent to delete instance
+              const instanceDeletetionReq: any = await (
+                await fetch(`${process.env.LXD_AGENT_SERVER}/api/v1/instance`, {
+                  method: "DELETE",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ id: job.data.name }),
+                })
+              ).json();
 
-                    // delete data from instance table
-                    const [deleteInstance]: any = await pool.query('DELETE FROM instances WHERE id=?', [job.data.name]);
+              if (instanceDeletetionReq.status !== 200) {
+                throw new Error("LXD can not create operation");
+              }
 
-                    // release user plan from in_use
-                    const [releaseUserPlan]: any = await pool.query('UPDATE user_plans SET in_use=0 WHERE id=?', [job.data.planId]);
+              // update status to deleting
+              const [setStatusToDeleting]: any = await pool.query(
+                "UPDATE instances SET status=? WHERE id=?",
+                ["deleting", job.data.name],
+              );
+            }
 
-                    // release reserved IP
-                    const [releaseIP]: any = await pool.query('UPDATE ip_addresses SET in_use=0 WHERE id=?', [job.data.instanceIPID]);
+            // ---
 
-                } else {
+            throw new Error(
+              "delete operation success, retrying for database clean up",
+            );
+          }
 
-                    // --- ignore when retry
-
-                    if (job.attemptsMade < 1) {
-                        // send data to lxd agent to delete instance
-                        const instanceDeletetionReq: any = await (await fetch(`${process.env.LXD_AGENT_SERVER}/api/v1/instance`, {
-                            method: "DELETE",
-                            headers: {
-                                "Content-Type": "application/json"
-                            },
-                            body: JSON.stringify({ id: job.data.name })
-                        })).json();
-
-                        if (instanceDeletetionReq.status !== 200) {
-                            throw new Error("LXD can not create operation");
-                        }
-
-                        // update status to deleting
-                        const [setStatusToDeleting]: any = await pool.query('UPDATE instances SET status=? WHERE id=?', ["deleting", job.data.name]);
-
-                    }
-
-                    // ---
-
-                    throw new Error("delete operation success, retrying for database clean up");
-
-                }
-
-                break;
-        }
-
-
-
+          break;
+      }
     } catch (error: any) {
-        throw new Error("Instance operation failed, Reason:", error);
+      throw new Error("Instance operation failed, Reason:", error);
     }
+  },
+  redisConnection,
+);
 
-}, redisConnection);
+worker.on("ready", () => {
+  iscrashed = false;
+});
+
+worker.on("error", () => {
+  console.log("DOWN");
+  iscrashed = true;
+});
+
+setInterval(() => {
+  const payload = {
+    status: iscrashed ? "DOWN" : "OK",
+    lastCheck: Date.now(),
+  };
+  redis.set(
+    "health:workers:lifecycle-worker",
+    JSON.stringify(payload),
+    "EX",
+    15,
+  );
+}, 5000);
